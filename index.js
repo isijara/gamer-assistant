d566182703e1c33a884a012dccb1a024ec3027ab
Ramón Alberto Isijara Medina <risijara@gmail.com>
2017-06-13 23:48:55 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const ApiAiApp = require('actions-on-google').ApiAiApp;
+
+exports.gamerBoy = (req, res) => {
+    
+    const app = new ApiAiApp({request: req, response: res});
+
+    // Create functions to handle requests here
+}
+
+
+
